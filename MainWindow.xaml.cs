b22091201e012a2f5bb37942faac32fcd61726b2
nagahole <rickwang121@gmail.com>
2020-11-18 19:08:47 +1300

    v2.0
    Added visual (colour toggle) and sound feedback. Added panic button and made it so
    
    - RMB will toggle off LMB autoclicker and vice versa
    - Only one side's autoclicker can be active at a time

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,11 +6,19 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using Autoclicker.Properties;
+using System.Windows.Media;
 
 namespace Autoclicker {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    public enum HotkeyType {
+        mouseButton,
+        key
+    }
+
     public partial class MainWindow : Window {
         #region stolen
         [System.Runtime.InteropServices.DllImport("user32.dll")]
@@ -43,124 +51,181 @@ namespace Autoclicker {
             mouse_event(MOUSEEVENTF_LEFTDOWN, (int) point.X, (int)point.Y, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, (int)point.X, (int)point.Y, 0, 0);
         }
-        
-        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void RightMouseClick() {
+            var point = GetMousePosition();
+
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, (int)point.X, (int)point.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, (int)point.X, (int)point.Y, 0, 0);
+        }
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        public const int MOUSEEVENTF_LEFTUP = 0x0004;
+
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        public const int MOUSEEVENTF_RIGHTUP = 0x0010;
         #endregion
 
         #region UI References
-        private System.Windows.Controls.TextBox minCpsTextbox;
-        private System.Windows.Controls.TextBox maxCpsTextbox;
-        private TextBlock hotkeyTextblock;
-        private System.Windows.Controls.CheckBox holdCheckbox;
+        private System.Windows.Controls.TextBox lmbMinCpsTextbox;
+        private System.Windows.Controls.TextBox lmbMaxCpsTextbox;
+        private TextBlock lmbHotkeyTextblock;
+
+        private System.Windows.Controls.TextBox rmbMinCpsTextbox;
+        private System.Windows.Controls.TextBox rmbMaxCpsTextbox;
+        private TextBlock rmbHotkeyTextblock;
+
+        private TextBlock panicButtonTextblock;
+
+        System.Windows.Controls.Button lmbButton, rmbButton;
 
         private const string
-            minCpsTextboxName = "MinCpsTextBox",
-            maxCpsTextboxName = "MaxCpsTextBox",
-            hotkeyTextblockName = "HotkeyText",
-            holdCheckboxName = "HoldCheckbox";
+            lmbMinCpsTextboxName = "LMBMinCpsTextBox",
+            lmbMaxCpsTextboxName = "LMBMaxCpsTextBox",
+            lmbHotkeyTextblockName = "LMBHotkeyText";
+
+        private const string
+            rmbMinCpsTextboxName = "RMBMinCpsTextBox",
+            rmbMaxCpsTextboxName = "RMBMaxCpsTextBox",
+            rmbHotkeyTextblockName = "RMBHotkeyText";
+
+        private const string
+            panicButtonTextblockName = "PanicButtonTextBlock";
+
+        private const string
+            lmbButtonName = "LMBButton",
+            rmbButtonName = "RMBButton";
+
+        private Brush toggledColor = new SolidColorBrush(Color.FromArgb(255, 39, 133, 43));
         #endregion
 
         private bool
-            toggled = false,
-            wait = true,
-            listening = false;
+            lmbToggled = false,
+            rmbToggled = false,
+            wait = true;
 
-        float maxCps, minCps;
-        int min, max;
+        float lmbMaxCps, lmbMinCps;
+        float rmbMaxCps, rmbMinCps;
 
-        #region Hotkeys
-        private enum HotkeyType {
-            mouseButton,
-            key
-        }
-
-        HotkeyType hotkeyType = HotkeyType.key;
-        Keys key = Keys.G;
-        MouseButtons mouseButton;
-        #endregion
+        HotkeyButton lmbHotkeyButton, rmbHotkeyButton, panicHotkeyButton;
 
         #region API / USEFUL CLASSES
-        private IKeyboardMouseEvents m_GlobalHook;
+        private static IKeyboardMouseEvents m_GlobalHook;
         private Random rand = new Random();
         #endregion
 
-        #region Input Listeners
-        public void Subscribe() {
-            m_GlobalHook = Hook.GlobalEvents();
-
-            m_GlobalHook.KeyDown += GlobalHookKeyDown;
-            m_GlobalHook.MouseClick += GlobalMousePress;
+        private void Exit() {
+            App.Current.Shutdown();
+            Process.GetCurrentProcess().Kill();
         }
-
-        private void GlobalHookKeyDown(object sender, KeyEventArgs e) {
-            if (listening) {
-                listening = false;
-                hotkeyType = HotkeyType.key;
-                key = e.KeyCode;
-                MatchHotkeyTextblockText();
-                return;
-            }
-
-            if (e.KeyCode == key && hotkeyType == HotkeyType.key) {
-                Toggle();
-            }
-        }
-
-        private void GlobalMousePress(object sender, System.Windows.Forms.MouseEventArgs e) {
-            if (listening) {
-                listening = false;
-                hotkeyType = HotkeyType.mouseButton;
-                mouseButton = e.Button;
-                MatchHotkeyTextblockText();
-                return;
-            }
-
-            if (hotkeyType == HotkeyType.mouseButton && mouseButton == e.Button) {
-                Toggle();
-            }
-        }
-
-        #endregion
 
         #region Initializations / Fired once
         public MainWindow() {
             InitializeComponent();
-            Subscribe();
             InitializeElements();
+            Subscribe();
+            HotkeyButton.Initialize();
+
+            InitializeHotkeyButtons();
 
             this.Closed += (s, e) => {
-                App.Current.Shutdown();
-                Process.GetCurrentProcess().Kill();
+                Exit();
             };
 
+            Thread lmbAc = new Thread(LMBAutoClick);
+            lmbAc.Start();
 
-            MatchHotkeyTextblockText();
-
-            Thread AC = new Thread(AutoClick);
-
-            AC.Start();
+            Thread rmbAc = new Thread(RMBAutoClick);
+            rmbAc.Start();
 
             Thread.Sleep(1);
             wait = false;
 
-            minCps = float.Parse(minCpsTextbox.Text);
-            maxCps = float.Parse(maxCpsTextbox.Text);
+            lmbMinCps = float.Parse(lmbMinCpsTextbox.Text);
+            lmbMaxCps = float.Parse(lmbMaxCpsTextbox.Text);
+
+            rmbMinCps = float.Parse(rmbMinCpsTextbox.Text);
+            rmbMaxCps = float.Parse(rmbMaxCpsTextbox.Text);
+
+        }
+
+        private void Subscribe() {
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseClick += GlobalHookMouseClick;
+        }
+
+        private void GlobalHookMouseClick(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left && rmbToggled) {
+                ToggleRMB();
+            } else if (e.Button == MouseButtons.Right && lmbToggled) {
+                ToggleLMB();
+            }
         }
 
         private void InitializeElements() {
-            minCpsTextbox = FindName(minCpsTextboxName) as System.Windows.Controls.TextBox;
-            maxCpsTextbox = FindName(maxCpsTextboxName) as System.Windows.Controls.TextBox;
-            hotkeyTextblock = FindName(hotkeyTextblockName) as TextBlock;
-            holdCheckbox = FindName(holdCheckboxName) as System.Windows.Controls.CheckBox;
+            lmbMinCpsTextbox = FindName(lmbMinCpsTextboxName) as System.Windows.Controls.TextBox;
+            lmbMaxCpsTextbox = FindName(lmbMaxCpsTextboxName) as System.Windows.Controls.TextBox;
+            lmbHotkeyTextblock = FindName(lmbHotkeyTextblockName) as TextBlock;
+
+            rmbMinCpsTextbox = FindName(rmbMinCpsTextboxName) as System.Windows.Controls.TextBox;
+            rmbMaxCpsTextbox = FindName(rmbMaxCpsTextboxName) as System.Windows.Controls.TextBox;
+            rmbHotkeyTextblock = FindName(rmbHotkeyTextblockName) as TextBlock;
+
+            panicButtonTextblock = FindName(panicButtonTextblockName) as TextBlock;
+
+            lmbButton = FindName(lmbButtonName) as System.Windows.Controls.Button;
+            rmbButton = FindName(rmbButtonName) as System.Windows.Controls.Button;
         }
 
-        private void AutoClick() {
+        private void InitializeHotkeyButtons() {
+            //LMB
+            lmbHotkeyButton = new HotkeyButton(Keys.G);
+            lmbHotkeyTextblock.Text = $"Press [{(lmbHotkeyButton.GetHotkey())}] to start";
+            lmbHotkeyButton.HotkeySetEvent += (sender, e) => {
+                lmbHotkeyTextblock.Text = $"Press [{(lmbHotkeyButton.GetHotkey())}] to start";
+            };
+            lmbHotkeyButton.HotkeyPressed += (sender, e) => {
+                ToggleLMB();
+            };
+            lmbHotkeyButton.SelectHotkeyEvent += (sender, e) => {
+                lmbHotkeyTextblock.Text = "[Press any key]";
+            };
+
+            //RMB
+
+            rmbHotkeyButton = new HotkeyButton(Keys.H);
+            rmbHotkeyTextblock.Text = $"Press [{(rmbHotkeyButton.GetHotkey())}] to start";
+            rmbHotkeyButton.HotkeySetEvent += (sender, e) => {
+                rmbHotkeyTextblock.Text = $"Press [{(rmbHotkeyButton.GetHotkey())}] to start";
+            };
+            rmbHotkeyButton.HotkeyPressed += (sender, e) => {
+                ToggleRMB();
+            };
+            rmbHotkeyButton.SelectHotkeyEvent += (sender, e) => {
+                rmbHotkeyTextblock.Text = "[Press any key]";
+            };
+
+            //Hotkey Button
+            panicHotkeyButton = new HotkeyButton(Keys.Multiply);
+            panicButtonTextblock.Text = $"Panic Button [{panicHotkeyButton.GetHotkey()}]";
+            panicHotkeyButton.HotkeySetEvent += (sender, e) => {
+                panicButtonTextblock.Text = $"Panic Button [{panicHotkeyButton.GetHotkey()}]";
+            };
+            panicHotkeyButton.HotkeyPressed += (sender, e) => {
+                Close();
+            };
+            panicHotkeyButton.SelectHotkeyEvent += (sender, e) => {
+                panicButtonTextblock.Text = "[Press any key]";
+            };
+        }
+
+        private void LMBAutoClick() {
             int carryover = 0;
             while (true) {
-                if (toggled) {
+                if (lmbToggled) {
                     //int delay = rand.Next(min, max + 1) - carryover;
-                    int delay = (int)System.Math.Round(1000f / (rand.NextDouble() * (maxCps - minCps) + minCps)) - carryover;
+                    int delay = (int)System.Math.Round(1000f / (rand.NextDouble() * (lmbMaxCps - lmbMinCps) + lmbMinCps)) - carryover;
 
                     DateTime start = DateTime.Now;
                     LeftMouseClick();
@@ -173,74 +238,166 @@ namespace Autoclicker {
                 }
             }
         }
+
+        private void RMBAutoClick() {
+            int carryover = 0;
+            while (true) {
+                if (rmbToggled) {
+                    //int delay = rand.Next(min, max + 1) - carryover;
+                    int delay = (int)System.Math.Round(1000f / (rand.NextDouble() * (rmbMaxCps - rmbMinCps) + rmbMinCps)) - carryover;
+
+                    DateTime start = DateTime.Now;
+                    RightMouseClick();
+                    while ((DateTime.Now - start).Milliseconds <= delay) {
+                        Thread.Sleep(1);
+                    }
+                    carryover = (DateTime.Now - start).Milliseconds - delay;
+                } else {
+                    Thread.Sleep(1);
+                }
+            }
+        }
         #endregion
 
         #region Recurring Methods
-        private void Toggle() {
-            if (minCps <= 0) {
-                minCpsTextbox.Text = "1";
+
+        private void ToggleLMB() {
+            if (lmbMinCps <= 0) {
+                lmbMinCpsTextbox.Text = "1";
             }
 
-            if (maxCps < minCps) {
-                maxCpsTextbox.Text = minCps.ToString();
+            if (lmbMaxCps < lmbMinCps) {
+                lmbMaxCpsTextbox.Text = lmbMinCps.ToString();
             }
 
-            toggled = !toggled;
+            lmbToggled = !lmbToggled;
+            rmbToggled = false;
 
-            MatchHotkeyTextblockText();
+            Console.Beep(lmbToggled ? 15000 : 8000, 1);
 
-            min = (int)System.Math.Round(1000f / (maxCps));
-            max = (int)System.Math.Round(1000f / (minCps));
-
-            Console.WriteLine(min + "." + max);
+            UpdateLMBVisuals();
+            UpdateRMBVisuals();
         }
 
-        private void MatchHotkeyTextblockText() {
-            hotkeyTextblock.Text = $"Press [" +
-                $"{(hotkeyType == HotkeyType.key? key.ToString() : mouseButton.ToString())}" +
+        private void ToggleRMB() {
+            if (rmbMinCps <= 0) {
+                rmbMinCpsTextbox.Text = "1";
+            }
+
+            if (rmbMaxCps < rmbMinCps) {
+                rmbMaxCpsTextbox.Text = rmbMinCps.ToString();
+            }
+
+            rmbToggled = !rmbToggled;
+            lmbToggled = false;
+
+            Console.Beep(rmbToggled ? 15000 : 8000, 1);
+
+            UpdateLMBVisuals();
+            UpdateRMBVisuals();
+        }
+
+        private void UpdateLMBVisuals() {
+            if (lmbToggled) {
+                lmbButton.Background = toggledColor;
+            } else {
+                lmbButton.ClearValue(BackgroundProperty);
+            }
+            
+            lmbHotkeyTextblock.Text = $"Press [" +
+                $"{lmbHotkeyButton.GetHotkey()}" +
                 $"] to " +
-                $"{(toggled? "stop" : "start")}";
+                $"{(lmbToggled? "stop" : "start")}";
+        }
+
+        private void UpdateRMBVisuals() {
+            if (rmbToggled) {
+                rmbButton.Background = toggledColor;
+            } else {
+                rmbButton.ClearValue(BackgroundProperty);
+            }
+
+            rmbHotkeyTextblock.Text = $"Press [" +
+                $"{rmbHotkeyButton.GetHotkey()}" +
+                $"] to " +
+                $"{(rmbToggled ? "stop" : "start")}";
         }
         #endregion
 
         #region UI Commands
-        private void ChangeHotkey_Click(object sender, RoutedEventArgs e) {
-            if (wait) return;
-            if (!listening) {
-                listening = true;
-                hotkeyTextblock.Text = "[Press a key]";
-            } 
+        private void LMB_ChangeHotkey_Click(object sender, RoutedEventArgs e) {
+            lmbHotkeyButton.OnClick();
         }
 
-        private void MinCpsTextBox_TextChanged(object sender, TextChangedEventArgs e) {
-            if (wait || minCps.ToString() == minCpsTextbox.Text) return;
+        private void RMB_ChangeHotkey_Click(object sender, RoutedEventArgs e) {
+            rmbHotkeyButton.OnClick();
+        }
 
-            string newText = minCpsTextbox.Text;
+        private void PanicButton_Click(object sender, RoutedEventArgs e) {
+            panicHotkeyButton.OnClick();
+        }
+
+        private void LMBMinCpsTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if (wait || lmbMinCps.ToString() == lmbMinCpsTextbox.Text) return;
+
+            string newText = lmbMinCpsTextbox.Text;
             float n;
 
             if (float.TryParse(newText, out n)) {
-                minCpsTextbox.Text = n.ToString();
-                minCps = n;
+                lmbMinCpsTextbox.Text = n.ToString();
+                lmbMinCps = n;
             } else if (newText == string.Empty) {
-                minCps = 0;
+                lmbMinCps = 0;
             } else {
-                minCpsTextbox.Text = minCps.ToString();
+                lmbMinCpsTextbox.Text = lmbMinCps.ToString();
             }
         }
 
-        private void MaxCpsTextBox_TextChanged(object sender, TextChangedEventArgs e) {
-            if (wait || maxCps.ToString() == maxCpsTextbox.Text) return;
+        private void LMBMaxCpsTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if (wait || lmbMaxCps.ToString() == lmbMaxCpsTextbox.Text) return;
 
-            string newText = maxCpsTextbox.Text;
+            string newText = lmbMaxCpsTextbox.Text;
             float n;
 
             if (float.TryParse(newText, out n)) {
-                maxCpsTextbox.Text = n.ToString();
-                maxCps = n;
+                lmbMaxCpsTextbox.Text = n.ToString();
+                lmbMaxCps = n;
             } else if (newText == string.Empty) {
-                maxCps = 0;
+                lmbMaxCps = 0;
             } else {
-                maxCpsTextbox.Text = maxCps.ToString();
+                lmbMaxCpsTextbox.Text = lmbMaxCps.ToString();
+            }
+        }
+
+        private void RMBMinCpsTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if (wait || rmbMinCps.ToString() == rmbMinCpsTextbox.Text) return;
+
+            string newText = rmbMinCpsTextbox.Text;
+            float n;
+
+            if (float.TryParse(newText, out n)) {
+                rmbMinCpsTextbox.Text = n.ToString();
+                rmbMinCps = n;
+            } else if (newText == string.Empty) {
+                rmbMinCps = 0;
+            } else {
+                rmbMinCpsTextbox.Text = rmbMinCps.ToString();
+            }
+        }
+
+        private void RMBMaxCpsTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if (wait || rmbMaxCps.ToString() == rmbMaxCpsTextbox.Text) return;
+
+            string newText = rmbMaxCpsTextbox.Text;
+            float n;
+
+            if (float.TryParse(newText, out n)) {
+                rmbMaxCpsTextbox.Text = n.ToString();
+                rmbMaxCps = n;
+            } else if (newText == string.Empty) {
+                rmbMaxCps = 0;
+            } else {
+                rmbMaxCpsTextbox.Text = rmbMaxCps.ToString();
             }
         }
         #endregion
---
+++ Properties/HotkeyButton.cs
@@ -0,0 +1,139 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using Autoclicker;
+
+namespace Autoclicker.Properties {
+    class HotkeyButton {
+        private HotkeyType hotkeyType;
+        private Keys key;
+        private MouseButtons mouseButton;
+
+        private static HotkeyButton listeningToButton = null;
+
+        public static bool listening => listeningToButton != null;
+
+        private static DateTime lastSet;
+
+        #region Events
+        public event EventHandler<SelectHotkeyEventArgs> SelectHotkeyEvent;
+
+        protected virtual void OnSelectHotkey(SelectHotkeyEventArgs e) {
+            SelectHotkeyEvent?.Invoke(this, e);
+        }
+
+        public event EventHandler<HotkeySetEventArgs> HotkeySetEvent;
+
+        protected virtual void OnHotkeySet(HotkeySetEventArgs e) {
+            HotkeySetEvent?.Invoke(this, e);
+        }
+
+        public event EventHandler<HotkeyPressedEventArgs> HotkeyPressed;
+
+        protected virtual void OnHotkeyPressed(HotkeyPressedEventArgs e) {
+            HotkeyPressed?.Invoke(this, e);
+        }
+        #endregion
+
+        private static IKeyboardMouseEvents m_GlobalHook;
+
+        public static void Initialize() {
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyDown += StaticGlobalHookKeyDown;
+            m_GlobalHook.MouseClick += StaticGlobalMousePress;
+        }
+
+        private static void StaticGlobalHookKeyDown(object sender, KeyEventArgs e) {
+            if (listening) {
+                listeningToButton.key = e.KeyCode;
+                listeningToButton.hotkeyType = HotkeyType.key;
+                HotkeySetEventArgs ev = new HotkeySetEventArgs();
+                listeningToButton.OnHotkeySet(ev);
+
+                listeningToButton = null;
+                lastSet = DateTime.Now;
+                return;
+            }
+        }
+
+        private static void StaticGlobalMousePress(object sender, MouseEventArgs e) {
+            if (listening) {
+                listeningToButton.mouseButton = e.Button;
+                listeningToButton.hotkeyType = HotkeyType.mouseButton;
+                HotkeySetEventArgs ev = new HotkeySetEventArgs();
+                listeningToButton.OnHotkeySet(ev);
+                listeningToButton = null;
+
+                lastSet = DateTime.Now;
+                return;
+            }
+        }
+
+        public HotkeyButton(Keys key) {
+            BaseConstructor();
+            this.key = key;
+            hotkeyType = HotkeyType.key;
+        }
+
+        public HotkeyButton(MouseButtons button) {
+            BaseConstructor();
+            mouseButton = button;
+            hotkeyType = HotkeyType.mouseButton;
+        }
+
+        private void BaseConstructor() {
+            m_GlobalHook.KeyDown += ListenForKeyDown;
+            m_GlobalHook.MouseClick += ListenForMouseClick;
+        }
+
+        private void ListenForKeyDown(object sender, KeyEventArgs e) {
+            if ((DateTime.Now - lastSet).Milliseconds < 10) {
+                return;
+            }
+            if(hotkeyType == HotkeyType.key && e.KeyCode == key && listeningToButton == null) {
+                HotkeyPressedEventArgs ev = new HotkeyPressedEventArgs();
+                OnHotkeyPressed(ev);
+            }
+        }
+
+        private void ListenForMouseClick(object sender, MouseEventArgs e) {
+            if ((DateTime.Now - lastSet).Milliseconds < 10) {
+                return;
+            }
+            if (hotkeyType == HotkeyType.mouseButton && e.Button == mouseButton && listeningToButton == null) {
+                HotkeyPressedEventArgs ev = new HotkeyPressedEventArgs();
+                OnHotkeyPressed(ev);
+            }
+        }
+
+        public void OnClick() {
+            if(listeningToButton == null) {
+                listeningToButton = this;
+            }
+            SelectHotkeyEventArgs e = new SelectHotkeyEventArgs();
+            OnSelectHotkey(e);
+        }
+
+        public string GetHotkey() {
+            return (hotkeyType == HotkeyType.key) ? key.ToString() : mouseButton.ToString();
+        }
+    }
+
+    public class SelectHotkeyEventArgs : EventArgs {
+
+    }
+
+    public class HotkeySetEventArgs : EventArgs {
+
+    }
+
+    public class HotkeyPressedEventArgs : EventArgs {
+
+    }
+}
